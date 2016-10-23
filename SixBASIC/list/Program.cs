f63e6dbc018a1380b50df98e054bb1f9d4a2238c
sixofdloc <sixofdloc@gmail.com>
2016-10-23 00:32:10 -0400

    Cleaning up

---
+++ SixBASIC/list/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SixBASIC;
+
+namespace list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage:  list filename.prg");
+            }
+            else
+            {
+                BASIC basic = new BASIC();
+                basic.Load(args[1]);
+                List<string> slist = basic.List();
+                foreach (string s in slist)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine("Press Any Key To Continue");
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
---
+++ SixBASIC/prg2bas/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SixBASIC;
+using System.IO;
+
+namespace prg2bas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+            }
+            else
+            {
+                BASIC basic = new BASIC();
+
+                if (basic.Load(args[0]))
+                {
+                    List<string> slist = basic.List();
+                    File.WriteAllLines(args[1], slist);
+                }
+                else
+                {
+                    Console.WriteLine("Load error: " + basic.LastError);
+                }
+                //Console.ReadLine();
+            }
+
+        }
+    }
+}
